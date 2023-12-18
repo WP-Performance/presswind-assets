4057a29cb4b3d497834dc5c6192499f1fc82cea2
ipatate <faramazpat@gmail.com>
2023-12-18 16:13:52 +0100

    clean code

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -2,24 +2,24 @@
 
 test('no slash start', function () {
     $path = 'test';
-    $clean_path = \PressWind\PWHelpers::cleanPath($path);
+    $clean_path = PressWind\PWHelpers::cleanPath($path);
     expect($clean_path)->toBe('/test/');
 });
 
 test('no slash end', function () {
     $path = '/test';
-    $clean_path = \PressWind\PWHelpers::cleanPath($path);
+    $clean_path = PressWind\PWHelpers::cleanPath($path);
     expect($clean_path)->toBe('/test/');
 });
 
 test('slash start and end', function () {
     $path = '/test/';
-    $clean_path = \PressWind\PWHelpers::cleanPath($path);
+    $clean_path = PressWind\PWHelpers::cleanPath($path);
     expect($clean_path)->toBe('/test/');
 });
 
 test('only slash at end', function () {
     $path = 'test/';
-    $clean_path = \PressWind\PWHelpers::cleanPath($path);
+    $clean_path = PressWind\PWHelpers::cleanPath($path);
     expect($clean_path)->toBe('/test/');
 });
